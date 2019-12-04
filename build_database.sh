15d1e48789e8647c455f3b56446f0558085b11ee
kebroderick <kevin.broderick@awacervices.com>
2019-12-04 15:18:19 +0000

    test

--- build_database.sh
+++ build_database.sh
@@ -2,10 +2,12 @@
 
 POSTGRES_USER=$1
 POSTGRES_HOST=$2
-POSTGRES_DB=$3
+POSTGRES_PORT=$3
+POSTGRES_DB=$4
 
 echo $POSTGRES_USER
 echo $POSTGRES_HOST
+echo $POSTGRES_PORT
 echo $POSTGRES_DB
 
 pattern_array=( "./ddl/schema/\w*.sql" "./ddl/table/\w*.\w*.sql" )
@@ -17,7 +19,7 @@ do
 	do
 		echo "Processing $FILE"
 		
-		psql  -v ON_ERROR_STOP=1 -X -h $POSTGRES_HOST -p 5432 -U $POSTGRES_USER -d $POSTGRES_DB -f $FILE
+		psql  -v ON_ERROR_STOP=1 -X -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -f $FILE
 
 		PSQL_EXIT_STATUS=$?	  
 		
