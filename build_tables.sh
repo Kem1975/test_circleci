8a9bb2190696303868a97165050584dc30c82ae4
kebroderick <kevin.broderick@awacervices.com>
2019-12-04 10:47:50 +0000

    test

--- build_tables.sh
+++ build_tables.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-find . | grep '~/tmp/ddl/table/*.*.sql' > files.txt
+echo "Process files"
+exit 99
 
-for FILE in $(cat files.txt)
-do
-	echo "Processing $f"
-	
-	psql -X -h localhost -p 5432 -U postgres -d ca_st -a -f $FILE
-
-	   psql_exit_status = $?	  
-
-	   if [ $psql_exit_status != 0 ]; then
-		 echo "psql failed while trying to run this sql script" 1>&2
-		 exit $psql_exit_status
-	   fi
-
-	   echo "sql script successful"
-	exit 0
-	
-done
+#find . | grep '~/tmp/ddl/table/*.*.sql' > files.txt
+#
+#for FILE in $(cat files.txt)
+#do
+#	echo "Processing $f"
+#	
+#	psql -X -h localhost -p 5432 -U postgres -d ca_st -a -f $FILE
+#
+#	   psql_exit_status = $?	  
+#
+#	   if [ $psql_exit_status != 0 ]; then
+#		 echo "psql failed while trying to run this sql script" 1>&2
+#		 exit $psql_exit_status
+#	   fi
+#
+#	   echo "sql script successful"
+#	exit 0
+#	
+#done
