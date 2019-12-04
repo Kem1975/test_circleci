65a7eb670c3dafee740357a1f5af46326b554a0e
kebroderick <kevin.broderick@awacervices.com>
2019-12-04 15:12:45 +0000

    test

--- build_database.sh
+++ build_database.sh
@@ -32,25 +32,5 @@ do
 		
 	done
 done
-#echo "Process table files..."
-#
-#for FILE in $(find . | grep '/ddl/table/\w*.\w*.sql')
-#do
-#	echo "Processing $FILE"
-#	
-#	psql  -v ON_ERROR_STOP=1 -X -h $POSTGRES_HOST -p 5432 -U $POSTGRES_USER -d $POSTGRES_DB -f $FILE
-#
-#	PSQL_EXIT_STATUS=$?	  
-#	
-#	echo $PSQL_EXIT_STATUS
-#
-#	if [ $PSQL_EXIT_STATUS != 0 ]; then
-#		echo "psql failed while trying to run this sql script" 1>&2
-#		exit $PSQL_EXIT_STATUS
-#	fi
-#
-#	echo "sql script successful"
-#	
-#done
 
 exit 0
