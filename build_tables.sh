a85c0df3310f78b27af3597aec2db0812fda9b09
kebroderick <kevin.broderick@awacervices.com>
2019-12-04 10:31:35 +0000

    test

--- build_tables.sh
+++ build_tables.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+
+$(find . | grep '~/tmp/ddl/table/*.*.sql')
+
 for FILE in $(find . | grep '~/tmp/ddl/table/*.*.sql')
 do
 	echo "Processing $f"
 	
 	psql -X -h localhost -p 5432 -U postgres -d ca_st -a -f $FILE
 
-	   psql_exit_status = $?
-	   psql_exit_status = 1
+	   psql_exit_status = $?	  
 
 	   if [ $psql_exit_status != 0 ]; then
 		 echo "psql failed while trying to run this sql script" 1>&2
