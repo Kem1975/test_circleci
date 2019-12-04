a1a38d7891e20adb7e2978a4503ab39041b450b1
kebroderick <kevin.broderick@awacervices.com>
2019-12-04 14:27:29 +0000

    test

--- ddl/table/mic_policy_aw.test_bad_table.sql
+++ ddl/table/mic_policy_aw.test_bad_table.sql
@@ -1,6 +1,2 @@
-CREATE TABLE IF NOT EXISTS mic_policy_aw.test_bad_table (
-	mad_gid NUMBERIX(22,0),
-	mad_entity_type varchar(64),
-	mad_add_entity_type varchar(64)
-);
+sdklfjg;lsdifjg'sipjersklnerg;nj
 
